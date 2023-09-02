6ec47f0cd1f975b9a2749481f650b354993dde90
Huzaifa Imran <51008597+huzaifa003@users.noreply.github.com>
2023-09-02 16:55:54 +0000

    Fixed Few Warnings

--- model.py
+++ model.py
@@ -6,8 +6,6 @@ from haystack.pipelines import DocumentSearchPipeline
 from haystack.utils import print_documents
 from haystack.pipelines import ExtractiveQAPipeline
 from haystack.utils import print_answers
-from haystack.pipelines import DocumentSearchPipeline
-from haystack.utils import print_documents
 from haystack.pipelines import Pipeline
 from haystack.nodes import JoinDocuments
 
@@ -33,34 +31,36 @@ document_store.update_embeddings(embedding_retriever, update_existing_embeddings
 # Initialize Reader
 reader = FARMReader(model_name_or_path="deepset/roberta-base-squad2")
 
-def start():
-    global document_store
-    global bm25_retriever
-    global embedding_retriever
-    global reader
-    global p_retrieval
-    document_store = InMemoryDocumentStore(use_bm25=True)
-    bm25_retriever = BM25Retriever()
-    p_retrieval = DocumentSearchPipeline(bm25_retriever)
-    # Initialize Sparse Retriever
-    print(document_store)
+# def start():
+#     global document_store
+#     global bm25_retriever
+#     global embedding_retriever
+#     global reader
+#     global p_retrieval
+#     document_store = InMemoryDocumentStore(use_bm25=True)
+#     bm25_retriever = BM25Retriever()
+#     p_retrieval = DocumentSearchPipeline(bm25_retriever)
+#     # Initialize Sparse Retriever
+#     print(document_store)
 
-    doc_dir = "/content/data/tutorial11"
-    got_docs = convert_files_to_docs(dir_path=doc_dir, clean_func=clean_wiki_text, split_paragraphs=True)
-    # document_store.delete_documents()
-    document_store.write_documents(got_docs)
-    bm25_retriever = BM25Retriever(document_store=document_store)
-    # Initialize embedding Retriever
-    embedding_retriever = EmbeddingRetriever(
-        document_store=document_store, embedding_model="sentence-transformers/multi-qa-mpnet-base-dot-v1"
-    )
-    document_store.update_embeddings(embedding_retriever, update_existing_embeddings=False)
+#     doc_dir = "/content/data/tutorial11"
+#     got_docs = convert_files_to_docs(dir_path=doc_dir, clean_func=clean_wiki_text, split_paragraphs=True)
+#     # document_store.delete_documents()
+#     document_store.write_documents(got_docs)
+#     bm25_retriever = BM25Retriever(document_store=document_store)
+#     # Initialize embedding Retriever
+#     embedding_retriever = EmbeddingRetriever(
+#         document_store=document_store, embedding_model="sentence-transformers/multi-qa-mpnet-base-dot-v1"
+#     )
+#     document_store.update_embeddings(embedding_retriever, update_existing_embeddings=False)
 
-    # Initialize Reader
-    reader = FARMReader(model_name_or_path="deepset/roberta-base-squad2")
+#     # Initialize Reader
+#     reader = FARMReader(model_name_or_path="deepset/roberta-base-squad2")
 
 def documentSearch(query):
-  global embedding_retriever
+#   global embedding_retriever
+  
+  global p_retrieval
   p_retrieval = DocumentSearchPipeline(embedding_retriever)
   res = p_retrieval.run(query=query, params={"Retriever": {"top_k": 5}})
   print_documents(res, max_text_len=500)
@@ -69,10 +69,10 @@ def documentSearch(query):
 def questionAnswer(question):
   
   # Create ensembled pipeline
-  global document_store
-  global bm25_retriever
-  global embedding_retriever
-  global reader
+#   global document_store
+#   global bm25_retriever
+#   global embedding_retriever
+#   global reader
   p_ensemble = Pipeline()
   p_ensemble.add_node(component=bm25_retriever, name="BM25Retriever", inputs=["Query"])
   p_ensemble.add_node(component=embedding_retriever, name="EmbeddingRetriever", inputs=["Query"])
@@ -94,8 +94,8 @@ def questionAnswer(question):
    
 
 def extractiveQA(query):
-  global reader
-  global bm25_retriever
+#   global reader
+#   global bm25_retriever
   p_extractive_premade = ExtractiveQAPipeline(reader=reader, retriever=bm25_retriever)
   res = p_extractive_premade.run(
       query=query, params={"Retriever": {"top_k": 10}, "Reader": {"top_k": 5}}
